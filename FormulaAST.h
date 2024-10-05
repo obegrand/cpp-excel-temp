08ccbe83711a2ceb891f3a53012ccb2347391d75
obegrand <obevan7777@yandex.ru>
2024-10-05 12:22:08 +0300

    ref 2.5

--- FormulaAST.h
+++ FormulaAST.h
@@ -6,28 +6,46 @@
 #include <forward_list>
 #include <functional>
 #include <stdexcept>
+#include <optional>
 
 namespace ASTImpl {
-class Expr;
+	class Expr;
 }
 
 class ParsingError : public std::runtime_error {
 	using std::runtime_error::runtime_error;
 };
 
+using CellFunction = std::optional<std::function<double(const Position&)>>;
+
 class FormulaAST {
 public:
-	explicit FormulaAST(std::unique_ptr<ASTImpl::Expr> root_expr);
+	explicit FormulaAST(std::unique_ptr<ASTImpl::Expr> root_expr,
+		std::forward_list<Position> cells);
 	FormulaAST(FormulaAST&&) = default;
 	FormulaAST& operator=(FormulaAST&&) = default;
 	~FormulaAST();
 
-	double Execute() const;
+	double Execute(CellFunction cellfunc) const;
+	void PrintCells(std::ostream& out) const;
 	void Print(std::ostream& out) const;
 	void PrintFormula(std::ostream& out) const;
 
+	std::forward_list<Position>& GetCells() {
+		return cells_;
+	}
+
+	const std::forward_list<Position>& GetCells() const {
+		return cells_;
+	}
+
 private:
 	std::unique_ptr<ASTImpl::Expr> root_expr_;
+	
+	// physically stores cells so that they can be
+	// efficiently traversed without going through
+	// the whole AST
+	std::forward_list<Position> cells_;
 };
 
 FormulaAST ParseFormulaAST(std::istream& in);
--- cell.cpp
+++ cell.cpp
@@ -4,22 +4,19 @@
 #include <iostream>
 #include <string>
 #include <optional>
+#include <utility>
 
-// Реализуйте следующие методы
-Cell::Cell() {
-	impl_ = std::make_unique<EmptyImpl>();
-}
+#include "common.h"
 
-Cell::~Cell() {
-	// тут вроде ненадо ничего делать?
-}
+Cell::Cell(SheetInterface& sheet) : impl_(std::make_unique<EmptyImpl>()), sheet_(sheet) {}
+Cell::~Cell() = default;
 
 void Cell::Set(std::string text) {
 	if (text.size() == 0) {
 		impl_ = std::make_unique<EmptyImpl>();
 	}
 	else if (text.size() > 1 && text[0] == '=') {
-		impl_ = std::make_unique<FormulaImpl>(std::move(text));
+		impl_ = std::make_unique<FormulaImpl>(std::move(text.erase(0, 1)), sheet_);
 	}
 	else {
 		impl_ = std::make_unique<TextImpl>(std::move(text));
@@ -27,81 +24,33 @@ void Cell::Set(std::string text) {
 }
 
 void Cell::Clear() {
-	impl_ = std::make_unique<EmptyImpl>();
+	cache_ = nullptr;
+	impl_ = nullptr;
 }
 
 Cell::Value Cell::GetValue() const {
-	return impl_->GetValue();
+	assert(impl_ != nullptr);
+
+	if (!HasCache()) cache_ = std::make_unique<Value>(impl_->GetValue());
+
+	return *cache_;
 }
 
 std::string Cell::GetText() const {
+	assert(impl_ != nullptr);
 	return impl_->GetText();
 }
 
-class Impl {
-public:
-	virtual Value GetValue() const = 0;
-	virtual std::string GetText() const = 0;
-protected:
-	Value value_;
-	std::string text_;
-};
+std::vector<Position> Cell::GetReferencedCells() const {
+	assert(impl_ != nullptr);
+	return impl_->GetReferencedCells();
+}
 
-class EmptyImpl : public Impl {
-public:
-	EmptyImpl() {
-		value_ = "";
-		text_ = "";
-	}
+void Cell::ClearCache() {
+	cache_ = nullptr;
+}
 
-	Value GetValue() const override {
-		return value_;
-	}
+bool Cell::HasCache() const {
+	return cache_ != nullptr;
+}
 
-	std::string GetText() const override {
-		return text_;
-	}
-};
-
-class TextImpl : public Impl {
-public:
-	TextImpl(std::string text) {
-		text_ = text;
-		if (text[0] == '\'') {
-			text = text.substr(1);
-		}
-		value_ = text;
-	}
-
-	Value GetValue() const override {
-		return value_;
-	}
-
-	std::string GetText() const override {
-		return text_;
-	}
-};
-
-class FormulaImpl : public Impl {
-public:
-	FormulaImpl(std::string expression) {
-		expression = expression.substr(1);
-		formula_ = ParseFormula(expression);
-		text_ = "=" + formula_->GetExpression();
-		value_ = expression;
-	}
-
-	CellInterface::Value GetValue() const override {
-		FormulaInterface::Value temp_value = formula_->Evaluate();
-		if (std::holds_alternative<double>(temp_value)) {
-			return std::get<double>(temp_value);
-		}
-		return std::get<FormulaError>(temp_value);
-	}
-
-	std::string GetText() const override {
-		return text_;
-	}
-private:
-	std::unique_ptr<FormulaInterface> formula_;
-};
